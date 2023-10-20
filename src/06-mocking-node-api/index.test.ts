e8004f1db5704a56e2416b4899518bca3f78e100
EvgeniaM6 <evgminukova6@gmail.com>
2023-10-20 14:51:03 +0300

    feat: add tests for mocking-node-api file

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,5 +1,10 @@
-// Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import path, { join } from 'path';
+
+const time = 1000;
+const cbFunc = jest.fn(() => {
+  console.log('');
+});
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -11,11 +16,20 @@ describe('doStuffByTimeout', () => {
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    jest.spyOn(global, 'setTimeout');
+    doStuffByTimeout(cbFunc, time);
+
+    expect(setTimeout).toHaveBeenLastCalledWith(cbFunc, time);
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    jest.spyOn(global, 'setTimeout');
+    doStuffByTimeout(cbFunc, time);
+
+    expect(cbFunc).not.toBeCalled();
+
+    jest.runAllTimers();
+    expect(cbFunc).toBeCalled();
   });
 });
 
@@ -29,24 +43,41 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    jest.spyOn(global, 'setInterval');
+    doStuffByInterval(cbFunc, time);
+
+    expect(setInterval).toHaveBeenLastCalledWith(cbFunc, time);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    jest.spyOn(global, 'setInterval');
+    doStuffByInterval(cbFunc, time);
+
+    jest.runOnlyPendingTimers();
+    expect(cbFunc.mock.calls.length).not.toBeLessThan(1);
   });
 });
 
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
-    // Write your test here
+    jest.spyOn(path, 'join');
+    await readFileAsynchronously('../05-partial-mocking/index.ts');
+
+    expect(join).toHaveBeenCalled();
   });
 
   test('should return null if file does not exist', async () => {
-    // Write your test here
+    const result = await readFileAsynchronously('../05-partial-mocking/ggg.ts');
+
+    expect(result).toBeNull();
   });
 
   test('should return file content if file exists', async () => {
-    // Write your test here
+    const result = await readFileAsynchronously(
+      '../05-partial-mocking/index.ts',
+    );
+
+    const containStr = (result as string).includes('unmockedFunction');
+    expect(containStr).toBeTruthy();
   });
 });
